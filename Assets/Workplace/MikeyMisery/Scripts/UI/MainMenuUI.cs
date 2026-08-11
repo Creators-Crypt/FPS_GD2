1dd2ce9f42ae4e83a92a0d79a33c699417726d63
Jamie S <jgsaker@student.fullsail.edu>
2026-08-11 17:18:17 -0500

    Revert "Merge branch 'Development' into masterj5452"
    This reverts commit fbee23f2379a81c828283ffb8f1f8f5eaa01ae19, reversing
    changes made to 60a9c4eaea4c395ba8ce21a2ffeee4ec43578bf1.

--- Assets/Workplace/MikeyMisery/Scripts/UI/MainMenuUI.cs
+++ Assets/Workplace/MikeyMisery/Scripts/UI/MainMenuUI.cs
@@ -3,25 +3,8 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuUI : MonoBehaviour
 {
-
-    public void NewGame()
-    {
-        SceneManager.LoadScene("LoadingScreen");
-    }
-
     public void OpenSettings()
     {
         SceneManager.LoadScene("SettingsMenu");
-    }
-
-    public void QuitGame()
-    {
-        Debug.Log("Quit Game");
-        Application.Quit();
-    }
-
-    public void OpenCredits()
-    {
-        SceneManager.LoadScene("CreditsMenu");
     }
 }
